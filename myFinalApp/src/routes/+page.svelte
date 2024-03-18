b92f03e6a56d905bb2118f79ac12a3afdf66c304
SantiagoVasquezSanchez <78321435+SantiagoVasquezSanchez@users.noreply.github.com>
2024-03-18 18:38:21 -0500

    Add files via upload

---
+++ myFinalApp/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Cachorro from './SelectorUsuario.svelte'
+</script>
+<h1>Hello dogs</h1>
+<Cachorro/>
+
+    
---
+++ myFinalApp/src/routes/SelectorUsuario.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let src = 'https://www.anipedia.net/imagenes/que-comen-los-perros.jpg';
+    export let name = 'cachorro';
+    const guardarDato =(newScr: string, newAlt: string) =>{
+        src = newScr;
+        name = newAlt;
+    };
+</script>
+<div>
+    <button on:click={() => guardarDato("https://th.bing.com/th/id/OIP.r_q4AfKhVxpfX5V8lfQJZAHaEK?pid=ImgDet&w=474&h=266&rs=1","sonriendo")}> 
+        Ingresar</button>
+</div>
+<img {src} alt={name}/>
+
